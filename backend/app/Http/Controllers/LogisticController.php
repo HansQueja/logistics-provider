5d4e67e4720d87cb0849dba6e08e9e50c8f1f046
Hans Queja <hansqueja8@gmail.com>
2025-08-17 10:13:10 +0800

    Feat: Add logistics controller

---
+++ backend/app/Http/Controllers/LogisticController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\LogisticProvider;
+use App\Models\Driver;
+
+class LogisticController extends Controller
+{
+    public function assigned_drivers($id)
+    {
+        $drivers = Driver::where('provider_id', $id)->count();
+        return response()->json("hell yeah");
+    }
+}
