4908edda8279d3fad5828af23ddf1cf1b741aa84
Hans Queja <hansqueja8@gmail.com>
2025-08-19 12:08:20 +0800

    Feat: Add POST delivery route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,5 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LogisticController;
+use App\Http\Controllers\DeliveryController;
 
 Route::get('/logistics/{id}/assigned_drivers', [LogisticController::class, 'assigned_drivers']);
+
+# Heatmap
+Route::post('/logistics/{id}/delivery', [DeliveryController::class, 'create_delivery']);
